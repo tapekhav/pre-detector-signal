e34c784c0abdf502f6a4e1d603e5d55bf419e38f
TapekhaVA <tapehavladimir@gmail.com>
2023-10-24 09:14:55 +0300

    ahhahahahah

--- library/files/src/file_reader.cpp
+++ library/files/src/file_reader.cpp
@@ -1,2 +1,20 @@
 #include <file_reader.h>
 
+#include <iostream>
+
+bool FileReader::readFrame()
+{
+    std::string file_data;
+    _file >> file_data;
+
+    size_t length = file_data.length();
+    if (length != 11)
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < length; ++i)
+    {
+       // if ()
+    }
+}
