21c42bbdce354af78774efc898b8bb1f60779c22
TapekhaVA <tapehavladimir@gmail.com>
2023-11-07 11:39:49 +0300

    add noise with snr

--- model/radio/headers/signal-to-noise_ratio.h
+++ model/radio/headers/signal-to-noise_ratio.h
@@ -2,8 +2,7 @@
 #define PRE_DETECTOR_SIGNAL_SIGNAL_TO_NOISE_RATIO_H
 
 #include <random>
-
-static const double kDispersion = 0;
+#include <memory>
 
 /*! \class NoiseGenerator
 *   \brief Generate noise with normal distribution
@@ -11,7 +10,7 @@ static const double kDispersion = 0;
 class NoiseGenerator
 {
 public:
-    inline NoiseGenerator(): _rd(), _gen(_rd()), _distribution(0, kDispersion) {}
+    inline explicit NoiseGenerator(int sigma) : _rd(), _gen(_rd()), _distribution(0, sigma) {}
     inline double generate() { return _distribution(_gen); }
 
 private:
@@ -20,9 +19,34 @@ private:
     std::normal_distribution<double> _distribution;
 };
 
+/**
+ * Деление и умножение на 10 используются для преобразования отношения сигнал/шум из децибелов (dB) в линейную шкалу.
+ * В децибелах отношение сигнал/шум (SNR) выражается как логарифмическая величина, и чтобы перейти к линейной шкале,
+ * нужно выполнить следующие операции:
+
+1. Разделить значение в децибелах на 10, чтобы получить отношение в натуральных логарифмах (десятичные децибелы).
+2. Взять экспоненту полученного значения, чтобы перейти к линейной шкале.
+
+Формула для этого преобразования выглядит следующим образом:
+
+\[ \text{Linear Value} = 10^{\left(\frac{\text{dB Value}}{10}\right)} \]
+
+Таким образом, деление на 10 выполняет первый шаг, переводя значение из децибелов в десятичные децибелы,
+ а затем возведение в степень 10 выполняет второй шаг, переводя значение в линейную шкалу.
+ Это преобразование позволяет работать с отношением сигнал/шум в линейной форме при моделировании или анализе сигналов и шумов.*/
+
 class SNR
 {
+public:
+    SNR(double snr_dB, double noise_dB);
 
+    [[nodiscard]] inline double getSNRLinear() const { return _snr_linear; }
+
+    [[nodiscard]] inline double generate() const { return _generator->generate(); }
+private:
+    double _snr_linear;
+
+    std::unique_ptr<NoiseGenerator> _generator;
 };
 
 #endif //PRE_DETECTOR_SIGNAL_SIGNAL_TO_NOISE_RATIO_H
--- model/radio/src/signal-to-noise_ration.cpp
+++ model/radio/src/signal-to-noise_ration.cpp
@@ -1,1 +1,10 @@
+#include <signal-to-noise_ratio.h>
 
+
+SNR::SNR(double snr_dB, double noise_dB)
+{
+    _snr_linear = std::pow(10, snr_dB / 10.0);
+    double noise_variance = std::pow(10, noise_dB / 10.0);
+
+    _generator = std::make_unique<NoiseGenerator>(std::sqrt(noise_variance));
+}
